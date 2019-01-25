e15315a386ffc30d13a1e0475d132374425ce658
Uxeron <vgabrielius123@gmail.com>
2019-01-26 01:48:29 +0200

    House moves from mass

--- Scripts/House.gd
+++ Scripts/House.gd
@@ -1,16 +1,35 @@
 extends Sprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var rotSpeed = 0
+var rotSpdDelta = 0.0005
+var leftMass = 0
+var rightMass = 0
+const houseWidth = 480
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
 func _physics_process(delta):
-	if Input.is_key_pressed(KEY_A):
-		rotate(-1.0 * delta)
-	if Input.is_key_pressed(KEY_D):
-		rotate(1.0 * delta)
+	rightMass = 0
+	leftMass = 0
+	
+	for body in $Area2D.get_overlapping_bodies():
+		if body.position.x > position.x:
+			rightMass += 1
+		else:
+			leftMass += 1
+
+	
+	#if Input.is_key_pressed(KEY_A):
+	if rightMass < leftMass:
+		rotSpeed -= rotSpdDelta;
+	#elif Input.is_key_pressed(KEY_D):
+	elif rightMass > leftMass:
+		rotSpeed += rotSpdDelta;
+	else:
+		if rotSpeed > 0:
+			rotSpeed -= rotSpdDelta;
+		elif rotSpeed < 0:
+			rotSpeed += rotSpdDelta;
+	
+	rotate(rotSpeed * delta)
