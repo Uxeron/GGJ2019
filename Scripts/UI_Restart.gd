4c84c7701164f2d01f80fd088ac0f85461b20632
Uxeron <vgabrielius123@gmail.com>
2019-01-26 15:17:36 +0200

    Changes to TouchScreenButton

--- Scripts/UI_Restart.gd
+++ Scripts/UI_Restart.gd
@@ -1,13 +1,29 @@
 extends TouchScreenButton
 
 var disabled = true
+var mouseTouching = false
 
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			if mouseTouching:
+				_on_Restart_button()
+"""
 func _physics_process(delta):
 	if is_pressed():
 		_on_Restart_button()
 		disabled = true
+"""
 
 func _on_Restart_button():
 	#get_node("/root/MainScene").reloadScene()
 	if disabled: return
 	get_node("/root/MainScene/Control/Transition").transition(get_viewport().get_texture(), global_position)
+
+
+func _on_Label_mouse_entered():
+	mouseTouching = true
+
+
+func _on_Label_mouse_exited():
+	mouseTouching = false
