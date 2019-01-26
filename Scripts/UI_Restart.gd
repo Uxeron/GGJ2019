5e7f46b07c3020a6ab0ee79088c7dd3c5783c62b
Uxeron <vgabrielius123@gmail.com>
2019-01-26 14:45:30 +0200

    Fixed restart button, other changes

--- Scripts/UI_Restart.gd
+++ Scripts/UI_Restart.gd
@@ -1,6 +1,8 @@
-extends TextureButton
+extends TouchScreenButton
 
+var disabled = true
 
-func _on_Restart_button_up():
+func _on_Restart_button():
 	#get_node("/root/MainScene").reloadScene()
-	get_node("/root/MainScene/Control/Transition").transition(get_viewport().get_texture(), rect_global_position)
+	if disabled: return
+	get_node("/root/MainScene/Control/Transition").transition(get_viewport().get_texture(), global_position)
