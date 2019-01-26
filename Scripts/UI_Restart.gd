0eaea9cc64f321efd9df45720f630535426e1909
Uxeron <vgabrielius123@gmail.com>
2019-01-26 14:57:56 +0200

    Changes to TouchScreenButton

--- Scripts/UI_Restart.gd
+++ Scripts/UI_Restart.gd
@@ -2,6 +2,11 @@ extends TouchScreenButton
 
 var disabled = true
 
+func _physics_process(delta):
+	if is_pressed():
+		_on_Restart_button()
+		disabled = true
+
 func _on_Restart_button():
 	#get_node("/root/MainScene").reloadScene()
 	if disabled: return
